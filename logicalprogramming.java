fdb2fb2409e6ae738a395896b80aaa8975f9c395
sheetalpatil24 <patilsheetal7913@gmail.com>
2022-09-09 00:01:34 +0530

    perfectnumber

--- logicalprogramming.java
+++ logicalprogramming.java
@@ -1,19 +1,40 @@
-// fibonacci series will start with 0 or 1
-//0 1 1 2 3 5 8 13 series formed by adding  last two numbers
+//perfect number is When the number is equal to the sum of its positive divisors excluding the number
 import java.util.Scanner;
-public class logicalprogramming {
-    public static void main(String[] args) {
-        int term ,a=0,b=1,c;
-        System.out.println("enter term");
-        Scanner r = new Scanner(System.in) ;
-        term =r.nextInt();
 
-    for(int i=1;i<=term;i++){
 
-        System.out.println(a+"");
-        c=a+b;
-        a=b;
-        b=c;
-  }
+public class logicalprogramming{
+public static void main(String[] args) {
+    int n, sum=0;
+    System.out.println("enter any number");
+    Scanner r= new Scanner(System.in);
+    n=r.nextInt();
+
+    for(int i=1; i<n; i++)
+    {
+      if(n%i==0)
+    {
+    sum= sum + i;
+    }
+ }
+    if (n==sum)
+    System.out.println("it is a perfect number");
+
+    else 
+    System.out.println("not a perfect number");
+   }
+
 }
-}
+  
+
+
+
+
+    
+    
+    
+        
+    
+     
+    
+      
+      
